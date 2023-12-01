f710fd76111b874abfb640e781e66d10cf1c17cf
Veligor <spaunroom@gmail.com>
2023-12-01 11:41:44 +0300

    sixteen lesson

---
+++ 16.js
@@ -0,0 +1,19 @@
+//Use the JavaScript Console to Check the Value of a Variable
+
+let a = 5;
+let b = 1;
+a++;
+// Only change code below this line
+
+
+let sumAB = a + b;
+console.log(a);
+
+//Use typeof to Check the Type of a Variable
+
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+console.log(typeof seven)
+console.log(typeof three)
