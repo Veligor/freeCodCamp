102cf15c3e981450bd73db3ff224050a842b0ed5
Veligor <spaunroom@gmail.com>
2023-12-04 14:36:04 +0300

    twenty eight

---
+++ 28.js
@@ -0,0 +1,59 @@
+//Generate an Array of All Object Keys with Object.keys()
+
+let users = {
+   Alan: {
+     age: 27,
+     online: false
+   },
+   Jeff: {
+     age: 32,
+     online: true
+   },
+   Sarah: {
+     age: 48,
+     online: false
+   },
+   Ryan: {
+     age: 19,
+     online: true
+   }
+ };
+ 
+ function getArrayOfUsers(obj) {
+   // Only change code below this line
+   let property = [];
+   property.push(Object.keys(obj))
+   return property;
+   // Only change code above this line
+ }
+ 
+ console.log(getArrayOfUsers(users));
+
+ let usersOne = {
+   Alan: {
+     age: 27,
+     online: false
+   },
+   Jeff: {
+     age: 32,
+     online: true
+   },
+   Sarah: {
+     age: 48,
+     online: false
+   },
+   Ryan: {
+     age: 19,
+     online: true
+   }
+ };
+ 
+ function getArrayOfUsers(obj) {
+   // Only change code below this line
+ 
+  return Object.keys(obj)
+  
+    // Only change code above this line
+ }
+ 
+ console.log(getArrayOfUsers(users));
