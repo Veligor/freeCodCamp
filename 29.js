ab10399a0a780e14867162e4cce6c3ed51b634d5
Veligor <spaunroom@gmail.com>
2023-12-04 17:22:26 +0300

    twenty nine lesson

---
+++ 29.js
@@ -0,0 +1,35 @@
+//Modify an Array Stored in an Object
+
+let user = {
+  name: "Kenneth",
+  age: 28,
+  data: {
+    username: "kennethCodesAllDay",
+    joinDate: "March 26, 2016",
+    organization: "freeCodeCamp",
+    friends: ["Sam", "Kira", "Tomo"],
+    location: {
+      city: "San Francisco",
+      state: "CA",
+      country: "USA",
+    },
+  },
+};
+
+function addFriend(userObj, friend) {
+  // Only change code below this line
+  userObj.data.friends.push(friend);
+  return userObj.data.friends;
+  // Only change code above this line
+}
+
+console.log(addFriend(user, "Pete"));
+
+//Convert Celsius to Fahrenheit
+
+function convertCtoF(celsius) {
+   let fahrenheit = (celsius * (9/5)) + 32;   // formula to convert from celsius to fahrenheit
+   return fahrenheit;
+ }
+ 
+ convertCtoF(30) ;
