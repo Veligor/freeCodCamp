e4ca7db52be648dd1a41926a14b31d30af895c47
Veligor <spaunroom@gmail.com>
2023-12-07 20:40:06 +0300

    fourty three lesson

---
+++ 43.js
@@ -0,0 +1,51 @@
+//Use a Mixin to Add Common Behavior Between Unrelated Objects
+
+let bird = {
+   name: "Donald",
+   numLegs: 2,
+ };
+ 
+ let boat = {
+   name: "Warrior",
+   type: "race-boat",
+ };
+ 
+ // Only change code below this line
+ let glideMixin = function (obj) {
+   obj.glide = function () {};
+ };
+ glideMixin(bird);
+ glideMixin(boat);
+ 
+ //Use Closure to Protect Properties Within an Object from Being Modified Externally
+ 
+ function Bird() {
+   let weight = 15;
+   this.getWeight = function () {
+     return weight;
+   };
+ }
+ 
+ //Understand the Immediately Invoked Function Expression (IIFE)
+ 
+ (function makeNest() {
+   console.log("A cozy nest is ready");
+ })();
+ 
+ //Use an IIFE to Create a Module.
+ 
+ let funModule = (function () {
+   return {
+     isCuteMixin: function (obj) {
+       obj.isCute = function () {
+         return true;
+       };
+     },
+     singMixin: function (obj) {
+       obj.sing = function () {
+         console.log("Singing to an awesome tune");
+       };
+     },
+   };
+ })();
+ 
