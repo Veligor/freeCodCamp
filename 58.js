364159861dd4d408ebce9c262dd88c922964c6df
Veligor <spaunroom@gmail.com>
2023-12-21 09:01:41 +0300

    fifty eight

---
+++ 58.js
@@ -0,0 +1,41 @@
+//Comparison with the Strict Equality Operator
+ 
+ // Setup
+ function testStrict(val) {
+   if (val === 7) { // Change this line
+     return "Equal";
+   }
+   return "Not Equal";
+ }
+ 
+//Practice comparing different values.
+
+function compareEquality(a, b) {
+   if (a === b) { // Change this line
+     return "Equal";
+   }
+   return "Not Equal";
+ }
+ 
+
+ //Comparison with the Inequality Operator
+
+
+function testNotEqual(val) {
+   if (val != 99) { // Change this line
+     return "Not Equal";
+   }
+   return "Equal";
+ }
+ 
+ //Comparison with the Strict Inequality Operator
+
+ // Setup
+function testStrictNotEqual(val) {
+   if (val !== 17) { // Change this line
+     return "Not Equal";
+   }
+   return "Equal";
+ }
+
+ 
