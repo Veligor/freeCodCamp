692db3ea785765e0f4ad7b3a279b88f0316d4e79
Veligor <spaunroom@gmail.com>
2023-12-21 08:59:45 +0300

    fifty seven lesson

---
+++ 57.js
@@ -0,0 +1,36 @@
+//Stand in Line
+
+function nextInLine(arr, item) {
+   // Only change code below this line
+   arr.push(item);
+   return arr.shift();
+   // Only change code above this line
+ }
+ 
+ //Understanding Boolean Values
+ 
+ function welcomeToBooleans() {
+   return true;
+ }
+ 
+ //Use Conditional Logic with If Statements
+ 
+ function trueOrFalse(wasThatTrue) {
+   // Only change code below this line
+   if (wasThatTrue) {
+     return "Yes, that was true";
+   }
+   return "No, that was false";
+   // Only change code above this line
+ }
+ 
+ //Comparison with the Equality Operator
+ 
+ function testEqual(val) {
+    if (val == 12) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+ 
