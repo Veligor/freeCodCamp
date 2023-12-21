ecf806ca19ce9c858843b8eb8b6bbc9294dc8272
Veligor <spaunroom@gmail.com>
2023-12-21 09:03:41 +0300

    fifty nine lesson

---
+++ 59.js
@@ -0,0 +1,41 @@
+//Comparison with the Greater Than Operator
+
+function testGreaterThan(val) {
+   if (val > 100) {  // Change this line
+     return "Over 100";
+   }
+ 
+   if (val > 10) {  // Change this line
+     return "Over 10";
+   }
+ 
+   return "10 or Under";
+}
+
+//Comparison with the Greater Than Or Equal To Operator
+
+function testGreaterOrEqual(val) {
+   if (val >= 20) {  // Change this line
+     return "20 or Over";
+   }
+ 
+   if (val >= 10) {  // Change this line
+     return "10 or Over";
+   }
+ 
+   return "Less than 10";
+ }
+ 
+
+ //Comparison with the Less Than Operator
+
+ function testLessThan(val) {
+   if (val < 25) {  // Change this line
+     return "Under 25";
+   }
+   if (val < 55) {  // Change this line
+     return "Under 55";
+   }
+   return "55 or Over";
+ }
+
